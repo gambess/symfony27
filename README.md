6ec23f7bfd6df9e610baaa8a173ddd3a5206e8a6
Raziel Valle Miranda <rvalle@ces.vocento.com>
2016-09-13 09:54:28 +0200

    RazielValle: Testing Javascript API Gigya with Symfony27

---
+++ README.md
@@ -0,0 +1,4 @@
+my_symfony27
+============
+
+A Symfony project created on September 8, 2016, 10:35 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+    public function accessAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/access.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+    public function registerAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/register.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+    
+    public function registerconfAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/registerconf.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+    public function recoverAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/recover.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+    public function verifyAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/verify.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+}
